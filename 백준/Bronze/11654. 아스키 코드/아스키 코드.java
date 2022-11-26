2afebcbd61e660df2d2133524361c4cc6e7d60a9
김세령 <106288108+kimseryeong@users.noreply.github.com>
2022-11-26 21:59:05 +0900

    [Bronze V] Title: 아스키 코드, Time: 196 ms, Memory: 17632 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// 아스키코드 
+		Scanner sc = new Scanner(System.in);
+		int zero = sc.next().charAt(0);
+		
+		System.out.println(zero);
+		
+	}
+
+}
