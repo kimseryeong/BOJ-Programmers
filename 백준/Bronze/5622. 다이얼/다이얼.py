593bb340d8404843a8578af500991f2491008e32
Seryeong <106288108+kimseryeong@users.noreply.github.com>
2023-07-19 11:12:43 +0900

    [Bronze II] Title: 다이얼, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.py
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -1,21 +1,9 @@
-dial = input()
+dial = ['ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ']
+val = input()
 sec = 0
 
-for i in dial:
-  if i == 'A' or i == 'B' or i == 'C':
-    sec += 3
-  elif i == 'D' or i == 'E' or i == 'F':
-    sec += 4
-  elif i == 'G' or i == 'H' or i == 'I':
-    sec += 5
-  elif i == 'J' or i == 'K' or i == 'L':
-    sec += 6
-  elif i == 'M' or i == 'N' or i == 'O':
-    sec += 7
-  elif i == 'P' or i == 'Q' or i == 'R' or i == 'S':
-    sec += 8
-  elif i == 'T' or i == 'U' or i == 'V':
-    sec += 9
-  elif i == 'W' or i == 'X' or i == 'Y' or i == 'Z':
-    sec += 10
+for i in range(len(val)):
+  for a in dial:
+    if val[i] in a:
+      sec += dial.index(a)+3
 print(sec)
