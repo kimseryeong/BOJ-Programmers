9043784c850f129f1fdb4a63ad8697dbae31e58b
Seryeong <106288108+kimseryeong@users.noreply.github.com>
2023-09-04 19:56:56 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 608 ms, Memory: 157524 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -1,16 +1,9 @@
-from bisect import bisect_left, bisect_right #이진탐색 모듈
 from sys import stdin
+from collections import Counter
+_ = stdin.readline()
+N = stdin.readline().split()
+_ = stdin.readline()
+M = stdin.readline().split()
 
-n = stdin.readline().rstrip()
-card = list(map(int, stdin.readline().split()))
-m = stdin.readline().rstrip()
-confirm = list(map(int, stdin.readline().split()))
-card.sort() #이진탐색을 위한 오름차순 정렬
-
-def count_by_range(a, left, right):
-  idxRight = bisect_right(a, right) #list에서 value가 들어갈 가장 오쪽 인덱스
-  idxLeft = bisect_left(a, left) #list에서 value가 들어갈 가장 왼쪽 인덱스
-  return idxRight - idxLeft
-
-for i in range(len(confirm)):
-  print(count_by_range(card, confirm[i], confirm[i]), end=' ')
+C = Counter(N)
+print(' '.join(f'{C[m]}' if m in C else '0' for m in M))
